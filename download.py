71c41da4eb934b5fa82f2c8c55902aaaffa1ca04
Tobias Brodd <tobias.brodd@gmail.com>
2021-03-29 20:13:44 +0200

    added support for loading pokedex from a file

--- download.py
+++ download.py
@@ -1,48 +1,70 @@
 from getopt import getopt, GetoptError
 from scraper import PokemonScraper, Dex
+from pokemon import Pokemon
+import pandas as pd
+import tools
 import sys
+
+HEADER = [
+    "no",
+    "name",
+    "type_1",
+    "type_2",
+    "stage",
+    "is_final",
+    "is_legendary",
+    "is_mythical",
+    "is_ultra",
+    "hp",
+    "attack",
+    "defense",
+    "sp_attack",
+    "sp_defense",
+    "speed",
+    "normal",
+    "fire",
+    "water",
+    "electric",
+    "grass",
+    "ice",
+    "fighting",
+    "poison",
+    "ground",
+    "flying",
+    "psychic",
+    "bug",
+    "rock",
+    "ghost",
+    "dragon",
+    "dark",
+    "steel",
+    "fairy",
+]
 
 
 def save(pokemon, dex=Dex.GALAR):
     file_name = f"data/pokemon_{dex.value}.csv"
     with open(file_name, "w") as f:
-        header = [
-            "no",
-            "name",
-            "type_1",
-            "type_2",
-            "stage",
-            "is_final",
-            "is_legendary",
-            "is_mythical",
-            "hp",
-            "attack",
-            "defense",
-            "sp_attack",
-            "sp_defense",
-            "speed",
-            "normal",
-            "fire",
-            "water",
-            "electric",
-            "grass",
-            "ice",
-            "fighting",
-            "poison",
-            "ground",
-            "flying",
-            "psychic",
-            "bug",
-            "rock",
-            "ghost",
-            "dragon",
-            "dark",
-            "steel",
-            "fairy",
-        ]
-        f.write(",".join(header) + "\n")
+        f.write(",".join(HEADER) + "\n")
         for p in pokemon:
             f.write(str(p) + "\n")
+
+
+def load(dex=Dex.GALAR):
+    pokemon = []
+    lines = []
+    file_name = f"data/pokemon_{dex.value}.csv"
+    with open(file_name, "r") as f:
+        lines = f.readlines()
+    header = lines[0].replace("\n", "").split(",")
+    for line in lines[1:]:
+        line = line.replace("\n", "")
+        values = line.split(",")
+        frame = pd.Series(values, index=header)
+        p = tools.frame_to_pokemon(frame)
+        pokemon.append(p)
+
+    return pokemon
 
 
 def main(argv):
@@ -80,6 +102,7 @@ def main(argv):
         print("Scraped:")
         print([p.name for p in pokemon])
 
+    # pokemon = load(dex=dex)
     save(pokemon, dex=dex)
 
 
